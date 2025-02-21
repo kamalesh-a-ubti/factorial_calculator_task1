b6d63db0e7bf7b20c910acb166d2ba031efb01ca
kamalesh-a-ubti <kamalesh.a@ubtiinc.com>
2025-02-21 17:42:51 +0530

    Factorialcalculated

--- Program.cs
+++ Program.cs
@@ -1,2 +1,32 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace FactorialCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the Non-Negative Number:");
+            int number = int.Parse(Console.ReadLine());
+            long factorial = CalculateFactorial(number);
+            Console.WriteLine($"The factorial of {number} is {factorial}");
+        }
+
+        static long CalculateFactorial(int n)
+        {
+            if (n < 0)
+            {
+                Console.WriteLine("The Number must be non-negative.");
+                return -1; // Return a value to indicate an error
+            }
+            else if (n == 0)
+            {
+                return 1;
+            }
+            else
+            {
+                return n * CalculateFactorial(n - 1);
+            }
+        }
+    }
+}
