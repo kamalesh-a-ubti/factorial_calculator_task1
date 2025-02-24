28e091c4d762e98ac403ec5b6c0d637420737167
kamalesh-a-ubti <kamalesh.a@ubtiinc.com>
2025-02-24 15:02:26 +0530

    comments

--- Program.cs
+++ Program.cs
@@ -4,11 +4,17 @@ namespace FactorialCalculator
 {
     class Program
     {
+
+        // Main Method
         static void Main(string[] args)
         {
+            //Taking input
             Console.WriteLine("Enter the Non-Negative Number:");
+            //converting the input into int
             int number = int.Parse(Console.ReadLine());
+            //calculating the factorial
             long factorial = CalculateFactorial(number);
+            //printing the output
             Console.WriteLine($"The factorial of {number} is {factorial}");
         }
 
@@ -16,15 +22,18 @@ namespace FactorialCalculator
         {
             if (n < 0)
             {
+                //neagtive condition
                 Console.WriteLine("The Number must be non-negative.");
                 return -1; // Return a value to indicate an error
             }
             else if (n == 0)
             {
+                //condition when nis zero 
                 return 1;
             }
             else
             {
+                //using recursive function to find factorial
                 return n * CalculateFactorial(n - 1);
             }
         }
